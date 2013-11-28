7a4fe0dce5b8357fd5edd80d07217a17bf4c79e3
groupd <groupd@groupd-virtual-machine.(none)>
2013-11-28 00:30:41 -0600

    Cleaned up Chunk Server

--- MushroomChunk.py
+++ MushroomChunk.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys, os, errno, getopt, logging, signal, re, argparse
+import netifaces
 
 try:
     import Pyro.core, Pyro.naming
@@ -12,74 +13,10 @@ Follow the instruction in the README file to install it, or go the Pyro
 webpage http://pyro.sourceforge.net.
 """
     sys.exit(1)
-#logging.basicConfig( filename='mushroom_chunk.log', level=logging.DEBUG )
-# Certificate validator class.
-class MushroomCertValidator(Pyro.protocol.BasicSSLValidator):
-    def __init__(self):
-        if os.path.isfile('authorized-clients'):
-            self.cfgfile = 'authorized-clients'
-        else:
-            sys.exit(1)
-
-        self.clients = self.clients_regexp = {}
-        
-        try:
-            # Read authorized-clients file.
-            f = open(self.cfgfile, 'r')
-            for line in f:
-                # Strip trailing end-line
-                line = line.rstrip('\n')
-                # Skip inline comments
-                line = re.sub('#.*', '', line)
-                # Strip spaces
-                line = line.strip()
-                # Skip empty lines
-                if line == '':
-                    continue
-                # Adding a new entry to the hash of authorized clients
-                regexp = re.match(r'^regexp:(.*)', line)
-                if regexp is None:
-                    self.clients[line] = True
-                else:
-                    self.clients_regexp[regexp.group(1)] = True
-            f.close()
-        except Exception, e:
-            raise e
-
-        Pyro.protocol.BasicSSLValidator.__init__(self)
-
-    def authorize(self, subject):
-        return (1, 0)
-
-    def deny(self, subject, code):
-        return (0, code)
-
-    def checkCertificate(self, cert):
-    
-        # Client must have a valid certificate
-        if cert is None:
-            return self.deny('NULL', 3)
-            
-        # Subject must match one of the subjects specified in the
-        # authorized-clients file
-        subject = str(cert.get_subject())
-        
-        if self.clients.has_key(subject):
-            return self.authorize(subject)
-        else:
-            # Check also if the subject matches one of the defined
-            # regular expressions
-            for m in self.clients_regexp.keys():
-                if re.match(m, subject):
-                    return self.authorize(subject)
-                    
-            return self.deny(subject, 4)
-
 
 # The chunk server class.
 class MushroomChunk( Pyro.core.ObjBase ):
     def __init__( self, root ):
-        #logging.DEBUG( 'Initialized CHunk Server' )
         self.root = os.path.abspath( root ) + '/'
         os.chdir( self.root )
         Pyro.core.ObjBase.__init__( self )
@@ -134,7 +71,6 @@ class MushroomChunk( Pyro.core.ObjBase ):
             
         return op_result
 
-    # TODO: This looks incorrect, look into it
     def access( self, path, mode ):
     
         try:
@@ -146,27 +82,18 @@ class MushroomChunk( Pyro.core.ObjBase ):
 # Main program.
 def main():
 
-    # Set default options.
-    hostname = '0.0.0.0'
-    port = 3637
-    foreground = False
-    secure = False
     mount_point = '/home/groupd/chunk'
     parser = argparse.ArgumentParser()
 
     parser.add_argument( 'mount_point', help="A directory or mount point must be specified" )
-    #parser.add_argument( '-v', '--version', action='store_true', help="Prints version information for the chunk server" )
-    #parser.add_argument( '-a', '--address', dest=hostname, help="Used to specify the ip address of the chunk server" )
-    #parser.add_argument( '-p', '--port', dest=port, help="User to specify the port the chunk server listens on" )
-    #parser.add_argument( '-s', '--secure', action='store_true', dest=foreground, help="Run in secure mode using x509 certificate authentication" )
-    #parser.add_argument( '-f', '--foreground', action='store_true', dest=secure, help="Run the chunk server in the foreground" )
+    parser.add_argumnet( 'server_name', help="The name used by Pyro to identify this chunk server" )
 
     args = parser.parse_args()
     mount_point = args.mount_point
+    server_name = args.server_name
     # Instantiate chunk server
     try:
         chunk_server = MushroomChunk( mount_point )
-        #logging.DEBUG( 'Mounted dir' )
     except:
         print >> sys.stderr, "Error: could not mount " + mount_point
         sys.exit(1)
@@ -175,8 +102,7 @@ def main():
     pid = 0
     
     try:
-        if not foreground:
-            pid = os.fork()
+        pid = os.fork()
     except OSError, e:
         raise Exception, "%s [%d]" % (e.strerror, e.errno)
         
@@ -192,21 +118,13 @@ def main():
         Pyro.core.initServer(banner=0)
 
         #get Pyro name server
-        ns=ns=Pyro.naming.NameServerLocator().getNS(host='192.168.1.22')
+        ns=Pyro.naming.NameServerLocator().getNS(host='192.168.1.22')
         
-        if secure:
-            daemon = Pyro.core.Daemon(prtcol='PYROSSL', host=hostname, port=port)
-            daemon.setNewConnectionValidator(MushroomCertValidator())
-        else:
-            #daemon = Pyro.core.Daemon(prtcol='PYRO', host=hostname, port=port)
-            import netifaces
-            ip = netifaces.ifaddresses('eth0')[2][0]['addr']
-            daemon = Pyro.core.Daemon('PYRO', ip)
-            daemon.useNameServer(ns)
+        ip = netifaces.ifaddresses('eth0')[2][0]['addr']
+        daemon = Pyro.core.Daemon('PYRO', ip)
+        daemon.useNameServer(ns)
             
-        # Use persistent connection (we don't want to use a Pyro
-        # nameserver, to keep the things simple).
-        uri = daemon.connect(chunk_server, 'MushroomChunk')
+        uri = daemon.connect( chunk_server, server_name )
         
         try:
             # Start the daemon.
