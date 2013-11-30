0074b2663fbab9e48bc15b3e0bd16338e608a17a
groupd <groupd@groupd-virtual-machine.(none)>
2013-11-30 00:25:51 -0600

    typos

--- MushroomMaster.py
+++ MushroomMaster.py
@@ -221,7 +221,7 @@ class MushroomMaster(Pyro.core.ObjBase):
         self.file_table[ path + 'size' ] = file_size
         logging.debug( 'File size in gen chunks' )
         logging.debug( file_size )
-        logging.debug( self.file_table[ path + 'size' ]
+        logging.debug( self.file_table[ path + 'size' ] )
         logging.debug( 'Wrote to file table: ' )
         logging.debug( self.file_table[ path ] )
         return chunk_ids
