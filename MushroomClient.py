3d6633922aafa72a23c8837976bbc890a0fe1475
groupd <groupd@groupd-virtual-machine.(none)>
2013-12-01 18:25:22 -0600

    ahhhh

--- MushroomClient.py
+++ MushroomClient.py
@@ -364,7 +364,6 @@ class MushroomClient(Fuse):
                             source_dict[ chunk_location ].append( chunk_id )
                             target_dict[ chunk_location ].append( ( uuid, target_name ) )
 
-                self.master_server.rename_chunks( source_dict, target_dict, source_path, target_path ) 
                 successful_chunk_rename = False
 
                 while not successful_chunk_rename:
@@ -399,6 +398,8 @@ class MushroomClient(Fuse):
                 self._reinitialize()
                 
         #return the read binary data to FUSE
+        self.master_server.rename_chunks( source_dict, target_dict, source_path, target_path ) 
+
         return None
 
             
